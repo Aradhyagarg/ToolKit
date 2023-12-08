85d98198b429da4d93e68b574d00434aef74fd8a
Aradhya Garg <aradhya0107.be20@chitkara.edu.in>
2023-12-08 17:56:44 +0530

    all

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { increment, incrementByValue, decrement } from './Reducers';
 
-const Home = ({value}) => {
+const Home = () => {
+    const dispatch = useDispatch();
+    const { c } = useSelector(state => state.custom);
 
-  return (
-    <div>
-        <h1>{value}</h1>
-        <button>Increment</button>
-        <button>Decrement</button>
-    </div>
-  )
-}
+    const addBtn = () => {
+        dispatch(increment());
+    };
 
-export default Home
+    const incrementBy = () => {
+        dispatch(incrementByValue(25));
+    };
+
+    const subBtn = () => {
+        dispatch(decrement());
+    };
+
+    return (
+        <div>
+            <h1>{c}</h1>
+            <button onClick={addBtn}>Increment</button>
+            <button onClick={incrementBy}>IncrementBy</button>
+            <button onClick={subBtn}>Decrement</button>
+        </div>
+    );
+};
+
+export default Home;
